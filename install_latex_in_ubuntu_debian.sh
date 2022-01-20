1fdae8d619d01004e6401ee539bec4ced0a909b4
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2022-01-20 19:46:31 +0800

    Several metropolis theme added

---
+++ install_latex_in_ubuntu_debian.sh
@@ -0,0 +1,8 @@
+sudo apt install -y --no-install-recommends \
+    texlive-latex-recommended \
+    texlive-latex-extra \
+    texlive-fonts-extra \
+    texlive-science \
+    texlive-pictures \
+    texlive-luatex
+
---
+++ metropolis_beamer/README.txt
@@ -0,0 +1,1 @@
+https://github.com/xu-cheng/latex-tutorial
